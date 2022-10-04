2843c100546a0fed288a896c179065a8cbe11954
w2leo <82013108+w2leo@users.noreply.github.com>
2022-10-04 18:43:49 +0300

    Added basic player movement
    Can move Player by touch or Mouse Down.
    Class PlayerMovement

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField]
+    private float playerSpeed = 5f;
+    private Vector2 targetPoint;
+
+    private void Update()
+    {
+        if (Input.touchCount > 0 && Input.touches[0].phase == TouchPhase.Began)
+        {
+            targetPoint = Camera.main.ScreenToWorldPoint(Input.touches[0].position);
+        }
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            targetPoint = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        }
+
+        if (Vector2.Distance(transform.position, targetPoint) > 0.1f)
+        {
+            MovePlayer(targetPoint);
+        }
+    }
+
+    void MovePlayer(Vector2 target)
+    {
+        Vector2 direction = (target - (Vector2)transform.position).normalized;
+        transform.Translate(direction * playerSpeed * Time.deltaTime);
+    }
+}
